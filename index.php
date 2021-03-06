06ce4b7d24294b44fe201084282ce791970e0160
Jean-Pierre Novak <jeanpierrenovak20@gmail.com>
2021-03-06 21:36:15 +0100

    Removed footer file and template folder

--- index.php
+++ index.php
@@ -19,16 +19,24 @@
 <!-- This page is manly here to switch between templates -->
 <body class="container-fluid">
     <main>
+            <!-- This is the code for the start menu -->
+            <section class="w-75 mx-auto text-center my-5">
 
-        <!-- This is the container for the start menu template -->
-        <section class="StartMenu w-75 mx-auto">
-            <?php
+                <!-- The name of the application and game -->
+                <h1>Connect 4</h1>
 
-                // Including the file with the start menu
-                include_once "templates/StartingMenu.php";
+                <!-- This section will contain the menu options -->
+                <section class="mt-5 pt-5">
 
-            ?>
-        </div>
+                    <!-- Option for playing connect 4 against a friend on the same device -->
+                    <button class="btn border-3 rounded-pill border-dark py-2 px-5 d-block mx-auto mt-3 friend">Play against friend</button>
+
+                    <!-- Option for playing connect 4 against a medium difficult AI system -->
+                    <button class="btn border-3 rounded-pill border-dark py-2 px-5 d-block mx-auto mt-3 AI">Play against computer</button>
+
+                </section>
+                
+            </section>
     </main>
 </body>
 <?php
