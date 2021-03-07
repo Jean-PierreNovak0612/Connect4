949ff278f8c8c1756a7c260c33c11f8df2a42069
Jean-Pierre Novak <jeanpierrenovak20@gmail.com>
2021-03-07 19:48:24 +0100

    Added players menu layout to page

--- subpages/playersOptions.php
+++ subpages/playersOptions.php
@@ -12,31 +12,58 @@
 
 <!-- This page is manly here to switch between templates -->
 <body class="container-fluid">
-    <main>
-            <!-- This is the code for the players options menu -->
-            <section class="w-75 mx-auto text-center my-5 plysoptions px-5">
+    <main class="text-center">
+        <!-- This is the code for the players options menu -->
+        <section class="w-75 mx-auto text-center my-5 plysoptions px-5">
 
-                <!-- The name of the application and game -->
-                <h1 class="w-100">Players options</h1>
+            <!-- The name of the application and game -->
+            <h1 class="w-100">Players options</h1>
 
-                <!-- This section will contain the menu options -->
-                <section class="mt-5 pt-5 me-5 w-25 d-inline-block text-start">
+            <!-- This section will contain the menu options -->
+            <section class="mt-5 pt-5 me-5 w-25 d-inline-block text-start">
 
-                    <!-- Creating the options field for the first player -->
-                    <label for="ply1name" class="form-label">Enter name for player 1:</label>
-                    <input type="text" class="form-control">
+                <!-- Creating the options field for the first player -->
+                <label for="ply1name" class="form-label">Enter name for player 1:</label>
+                <input type="text" class="form-control">
 
+                <!-- Creating color selection palet -->
+                <h4 class="text-start w-100 mt-5 mb-2">Select your chip color:</h4>
+                <section class="p-5 mt-3"> 
+                    <button id="blue" class="color1 rounded-circle btn "></button>
+                    <button id="red" class="color1 rounded-circle btn "></button>
+                    <button id="yellow" class="color1 rounded-circle btn "></button>
+                    <button id="green" class="color1 rounded-circle btn "></button>
                 </section>
 
-                <section class="mt-5 pt-5 w-25 ms-5 d-inline-block text-start">
+                <!-- Setting the ready button -->
+                <button class="ready btn border-3 border-dark rounded-pill py-2 px-5">Ready!</button>
 
-                    <!-- Creating the options field for the first player -->
-                    <label for="ply1name" class="form-label">Enter name for player 1:</label>
-                    <input type="text" class="form-control">
-
-                </section>
-                
             </section>
+
+            <section class="mt-5 pt-5 w-25 ms-5 d-inline-block text-start">
+
+                <!-- Creating the options field for the first player -->
+                <label for="ply1name" class="form-label">Enter name for player 2:</label>
+                <input type="text" class="form-control">
+
+                <!-- Creating color selection palet -->
+                <h4 class="text-start w-100 mt-5 mb-2">Select your chip color:</h4>
+                <section class="p-5 mt-3"> 
+                    <button id="blue" class="color2 rounded-circle btn "></button>
+                    <button id="red" class="color2 rounded-circle btn "></button>
+                    <button id="yellow" class="color2 rounded-circle btn "></button>
+                    <button id="green" class="color2 rounded-circle btn "></button>
+                </section>
+
+                <!-- Setting the ready button -->
+                <button class="ready btn border-3 border-dark rounded-pill py-2 px-5">Ready!</button>
+
+            </section>
+            
+        </section>
+
+        <!-- Button that allowes to start the game only when both players are ready -->
+        <button class="btn mt-5 border-3 border-dark rounded-pill py-2 px-5">Start game!</button>
     </main>
 </body>
 <script src="assets/js/transition.js"></script>
