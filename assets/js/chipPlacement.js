2830867ff59fe3b7ca11ca8d3b64f959626e6a24
Jean-Pierre Novak <jeanpierrenovak20@gmail.com>
2021-03-11 20:18:00 +0100

    AI blocks sideways and upside down

--- assets/js/chipPlacement.js
+++ assets/js/chipPlacement.js
@@ -1,15 +1,10 @@
 var notFull = [0, 1, 2, 3, 4, 5, 6];
 $(document).on('click', 'button.enterChip', function(){
     var id = $(this).attr('id');
-    var j = id%10;
-    var cls = 'btn enterChip rounded-circle border-3 border-dark';
+    var j = parseInt(id%10);
     var color = localStorage.getItem('ply1Color');
     setPlayerChip(color, j);
-    setAIChip(notFull);
-    for(i=0; notFull.length; i++){
-        if($('#0'+notFull[i]).attr('class') != cls){
-            notFull.splice(i, 1);
-            break;
-        }
-    }
+    notFull = CheckIfEmpty(notFull);
+    rowVictory(notFull);
+    notFull = CheckIfEmpty(notFull);
 })
--- assets/js/functions/singleplayer.js
+++ assets/js/functions/singleplayer.js
@@ -1,14 +1,17 @@
-function setPlayerChip(color, j){
+function setPlayerChip(color, j, score){
     var cls = 'btn enterChip rounded-circle border-3 border-dark';
     for(i=5; i>=0; i--){
-        id=i*10+j;
+        id=(i*10)+j;
         if($('#'+id).attr('class') == cls){
             $('#'+id).removeClass('enterChip');
             $('#'+id).addClass(color);
-            break;
+            $('#'+id).val('1')
+            return i;
         }else if($('#0'+j).attr('class') == cls && $('#1'+j).attr('class') != cls){
             $('#0'+j).removeClass('enterChip');
             $('#0'+j).addClass(color);
+            $('#0'+j).val('1')
+            return 0;
         }
     }
 }
@@ -29,10 +32,88 @@ function setAIChip(notFull){
         if($('#'+id).attr('class') == cls){
             $('#'+id).removeClass('enterChip');
             $('#'+id).addClass(color);
+            $('#'+id).val('5')
             break;
         }else if($('#0'+j).attr('class') == cls && $('#1'+j).attr('class') != cls){
             $('#0'+j).removeClass('enterChip');
             $('#0'+j).addClass(color);
+            $('#0'+j).val('5')
         }
+    }
+}
+function CheckIfEmpty(notFull){
+    var cls = 'btn enterChip rounded-circle border-3 border-dark';
+    for(i=0; notFull.length; i++){
+        if($('#0'+notFull[i]).attr('class') != cls){
+            notFull.splice(i, 1);
+            break;
+        }
+    }
+    return notFull;
+}
+function columnVictory(notFull){
+    placed = false;
+    for(i=0; i<7; i++){
+        for(j=0; j<3; j++){
+            var score = 0;
+            for(s=j; s<(j+4); s++){
+                score += parseInt($('#'+s+''+i).val());
+            }
+            if(score == 3 || score == 15){
+                for(s=j; s<(j+4); s++){
+                    $('#'+s+''+i).removeClass('enterChip');
+                    $('#'+s+''+i).addClass('yellow');
+                    $('#'+s+''+i).val('5');
+                    placed = true;
+                    break;
+                }
+                if(placed){
+                    break;
+                }
+            }
+            if(placed){
+                break
+            }
+        }
+    }
+    // if(!placed){
+    //     leftDiagonalVictory(notFull)
+    // }
+}
+function rowVictory(notFull){
+    placed = false;
+    for(i=0; i<6; i++){
+        for(j=0; j<4; j++){
+            var score = 0;
+            for(s=j; s<(j+4); s++){
+                score += parseInt($('#'+i+''+s).val());
+            }
+            if(score == 3 || score == 15){
+                for(s=j; s<(j+4); s++){
+                    if(i < 5){
+                        if($('#'+(i+1)+''+s).val() != '0' && $('#'+i+''+s).val() == '0'){
+                            $('#'+i+''+s).removeClass('enterChip');
+                            $('#'+i+''+s).addClass('yellow');
+                            $('#'+i+''+s).val('5');
+                            placed = true;
+                            break
+                        }
+                    }
+                    else if($('#'+i+''+s).val() == '0'){
+                        $('#'+i+''+s).removeClass('enterChip');
+                        $('#'+i+''+s).addClass('yellow');
+                        $('#'+i+''+s).val('5');
+                        placed = true;
+                        break;
+                    }
+                }
+                if(placed){
+                    break;
+                }
+            }
+        }
+    }
+    if(!placed){
+        columnVictory(notFull)
     }
 }
